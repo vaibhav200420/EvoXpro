b040266c665e5839e1669e6a4a1daea1fc9d1d76
Riya Sahu <152384568+Riyasahu2004@users.noreply.github.com>
2024-06-12 15:07:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/vaibhav200420/EvoXpro-Music",
+    "https://github.com/Riyasahu2004/EvoXpro",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
